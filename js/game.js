751e4d81ef92344b7eabe3df28cce11091dfe86e
Aditya Raikar <raikaraditya22@gmail.com>
2024-05-21 20:34:12 +0200

    fixed bug with images

--- js/game.js
+++ js/game.js
@@ -14,7 +14,7 @@ class Game {
         520,
         100,
         120,
-        "../images/naruto.gif"
+        "./images/naruto.gif"
       );
       this.height = 600
       this.width = 700
@@ -264,7 +264,7 @@ class Player {
           this.height = 150;
           this.element = document.createElement("img");
       
-          this.element.src = "../images/shuriken.gif";
+          this.element.src = "./images/shuriken.gif";
           this.element.style.position = "absolute";
           this.element.style.width = `${this.width}px`;
           this.element.style.height = `${this.height}px`;
@@ -324,7 +324,7 @@ class Player {
           this.height = 130;
           this.element = document.createElement("img");
       
-          this.element.src = "../images/ramen.png";
+          this.element.src = "./images/ramen.png";
           this.element.style.position = "absolute";
           this.element.style.width = `${this.width}px`;
           this.element.style.height = `${this.height}px`;
